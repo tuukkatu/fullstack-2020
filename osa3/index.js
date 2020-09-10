3c88d9852242ff416409524471ddb55918b57a78
Tuukka Turunen <tuukka.turunen@helsinki.fi>
2020-09-10 14:27:23 +0300

    init heroku

--- osa3/index.js
+++ osa3/index.js
@@ -109,7 +109,7 @@ app.get('/info', (req, res) => {
     res.send(`<p>Phonebook has info for ${personAmount} people</p><p>${time}</p>`)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
