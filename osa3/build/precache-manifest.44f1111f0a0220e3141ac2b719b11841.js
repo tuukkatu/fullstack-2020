b46beaf5b639f9532f5b91a719b76b7390ef1b48
Tuukka Turunen <tuukka.turunen@helsinki.fi>
2020-09-14 12:41:02 +0300

    pieniä muutoksia

---
+++ osa3/build/precache-manifest.44f1111f0a0220e3141ac2b719b11841.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3393e91ad21797b043e3be1c9c77b9f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "106c7f3a43abdc577df9",
+    "url": "/static/css/main.8a194a05.chunk.css"
+  },
+  {
+    "revision": "d472febbadbe6186733c",
+    "url": "/static/js/2.f4d6aeea.chunk.js"
+  },
+  {
+    "revision": "702a169563591bf225c05f56f2892240",
+    "url": "/static/js/2.f4d6aeea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "106c7f3a43abdc577df9",
+    "url": "/static/js/main.0df9e9cc.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
--- osa3/index.js
+++ osa3/index.js
@@ -4,6 +4,7 @@ const app = express()
 const cors = require('cors')
 
 app.use(express.json())
+app.use(express.static('build'))
 app.use(cors())
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
