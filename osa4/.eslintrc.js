06de6a628dca2bceaf4ed2caf5ca7de0ebd84745
Tuukka Turunen <tuukka.turunen@helsinki.fi>
2020-10-01 12:57:28 +0300

    teht. 4.1

---
+++ osa4/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  'env': {
+    'node': true,
+    'commonjs': true,
+    'es2021': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 12
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'no-console': 0,
+    'no-unused-vars': ['error', { 'vars': 'all', 'args': 'none' }]
+  }
+}
